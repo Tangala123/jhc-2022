7bc471b96015d928c9063c2504abce1be84ae0f4
Tangala123 <98816309+Tangala123@users.noreply.github.com>
2022-12-29 11:06:51 +0530

    Update tomcatdeploy.groovy

--- vars/tomcatdeploy.groovy
+++ vars/tomcatdeploy.groovy
@@ -1,5 +1,5 @@
 def call (ip,creds,user) {
-  sshagent(['creds']) {
+  sshagent(["creds"]) {
     sh "scp -o StrictHostKeyChecking=no target/*.war ${user}@${ip}:/opt/tomcat009/webapps"
     sh "ssh ${user}@${ip} /opt/tomcat009/bin/shutdown.sh"
     sh "ssh ${user}@${ip} /opt/tomcat009/bin/startup.sh"
